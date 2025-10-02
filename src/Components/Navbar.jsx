e6c102ab2aad19f722080524c5f5ee11191dfe02
Badsha012 <badshasheikh361@gmail.com>
2025-10-02 11:37:08 +0600

    second commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
@@ -26,17 +27,20 @@ const Navbar = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+        
+        <Link to='/'>Home</Link>
+        </li>
+         <li>
+        
+        <Link to='/contact'>Contact</Link>
+        </li>
+         <li>
+        
+        <Link to='/about'>About</Link>
+        </li>
+
+     
     </ul>
   </div>
   <div className="navbar-end">
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Home = () => {
     return (
         <div>
-            Wlecome home
+            <Navbar></Navbar>
+            <main className='min-h-[calc(100vh-287px)]'>Welcome my home pages</main>
+
+            <Footer></Footer>
         </div>
     );
 };
