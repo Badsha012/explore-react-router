7f9b18660956f3f45f90c932a7852b3aeda99a1d
Badsha012 <badshasheikh361@gmail.com>
2025-10-02 10:31:41 +0600

    ten commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+//import { Connect } from 'vite'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
 
 const router=createBrowserRouter([
 {
   path:'/',
   element:<App></App>
 },
+{
+  path:'/contact',
+  element:<Contact></Contact>
+},
+{
+  path:'/about',
+  element:<About></About>
+},
+
 
 ])
 
