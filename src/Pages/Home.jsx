d890982c8ae38326292f7c53681853669d2cf2d8
Badsha012 <badshasheikh361@gmail.com>
2025-10-02 16:38:17 +0600

    new file added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 
+
 const Home = () => {
+ const [plants,setPlants] =useState([])
+ useEffect(()=>{
+    fetch('https://openapi.programming-hero.com/api/plants')
+    .then(res => res.json())
+    .then(data => setPlants(data?.plants));
+  
+ }, [])
+console.log(plants);
+
     return (
         <div>
-            <Navbar></Navbar>
-            <main className='min-h-[calc(100vh-287px)]'>Welcome my home pages</main>
+            <p>Welcome Home</p>
+            <p>Plant Card</p>
+            {
+                plants.map(plant=>{
+                    <p>{plant.name}</p>
+                })
+            }
 
-            <Footer></Footer>
         </div>
     );
 };
