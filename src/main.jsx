d7a20aca345567122d35b80f4ea8320493413a82
Badsha012 <badshasheikh361@gmail.com>
2025-10-02 10:48:49 +0600

    seven commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import { createBrowserRouter, RouterProvider } from 'react-router'
 //import { Connect } from 'vite'
 import About from './Pages/About.jsx'
 import Contact from './Pages/Contact.jsx'
+import Home from './Pages/Home.jsx'
 
 const router=createBrowserRouter([
 {
-  path:'/',
-  element:<App></App>
+  index:true,
+  element:<Home></Home>
 },
 {
   path:'/contact',
